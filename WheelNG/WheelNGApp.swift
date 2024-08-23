81920ce67efe2417a1b92ada999243b88af70a9f
Stef1949 <stef1949.sr@gmail.com>
2024-08-23 11:41:50 +0100

    Initial Commit

---
+++ WheelNG/WheelNGApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelNGApp.swift
+//  WheelNG
+//
+//  Created by Stephan Ritchie on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WheelNGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
