dee2c84ee4d27feb91dafc311a26a581b0477604
Stef1949 <stef1949.sr@gmail.com>
2024-08-28 22:46:57 +0100

    New app logo

--- WheelNG/ContentView.swift
+++ WheelNG/ContentView.swift
@@ -20,19 +20,26 @@ struct ContentView: View {
         print("Connect tapped")
     }
     
-    //Settings Variables
+    //View Variables
     @State private var isDarkMode = true
     @State private var isHorizonLockEnabled = true
+    @State private var isInfoPopoverPresented: Bool = false
+    
         
     //RPM Variables
-    @State private var rpm = 5
-    private let minValue = 0.0
-    private let maxValue = 11.0
+    @State private var rpm = 5.0
+    @State private var minValue = 0.0
+    @State private var maxValue = 11.0
+    
+    //Boost Variables
+    @State private var minPressure = 0.0
+    @State private var maxPressure = 17.0
+    @State private var currentPressure = 4.3
     
     //Speed Variables
     @State private var minSpeed = 0.0
-    @State private var maxSpeed = 170.0
-    @State private var currentSpeed = 50.0
+    @State private var maxSpeed = 230.0
+    @State private var currentSpeed = 57.6
     
     let gradient = Gradient(colors: [.green, .yellow, .orange, .red])
     
@@ -50,6 +57,11 @@ struct ContentView: View {
     //Long Press Gesture
     @GestureState private var isDetectingLongPress = false
     @State private var completedLongPress = false
+    
+    // Speed Units
+    @State var selectedUnit = 1 // 1 for MPH, 2 for KM/H
+    
+    
     
     // Track Orientation
     
@@ -92,7 +104,7 @@ struct ContentView: View {
                 }
                 .foregroundColor(.gray)
                 .tint(gradient)
-                .padding([.top, .leading, .trailing])
+                .padding([.top, .leading, .trailing], 25.0)
                 
                 Image(systemName: "")
                 
@@ -107,64 +119,66 @@ struct ContentView: View {
                         // Gauge Cluster
                         ZStack {
                             // Speed Gauge
-                            Gauge(value: currentSpeed, in: 0...200) {
+                            Gauge(value: currentSpeed, in: minSpeed...maxSpeed) {
                                 Image(systemName: "gauge.medium")
                                     .font(.system(size: 50.0))
+                                
                             } currentValueLabel: {
                                 Text("\(currentSpeed.formatted(.number))")
-                                
                             }
-                            .gaugeStyle(SpeedometerGaugeStyle())
+                            .gaugeStyle(SpeedometerGaugeStyle(selectedUnit: selectedUnit))
                             .rotationEffect(.degrees(-90 - motionManager.angle), anchor: .center) // Apply rotation based on tilt
                             
-                            Spacer()
+                            .padding(.bottom, 25.0)
                             
                             //Boost Gauge
+                            let boostGradient = Gradient(colors: [.clear, .yellow, .orange])
+                            
                             VStack {
-                                Gauge(value: motionManager.angle, in: -90...90) {
+                                Gauge(value: currentPressure, in: minPressure...maxPressure) {
                                     Text("Speed")
                                 } currentValueLabel: {
-                                    Text(Int(currentSpeed), format: .number)
+                                    Text(Int(currentPressure), format: .number)
                                     
                                 } minimumValueLabel: {
-                                    Text("\(Int(minSpeed))")
+                                    Text("\(Int(minPressure))")
                                     
                                         .foregroundColor(.green)
                                 } maximumValueLabel: {
-                                    Text("\(Int(maxSpeed))")
+                                    Text("\(Int(maxPressure))")
                                     
                                 }
                                 .gaugeStyle(.accessoryCircular)
+                                .tint(boostGradient)
                                 
-                                Text("PSI")
-                                    .offset(CGSize(width: 0, height: -12))
                             }
-                            .offset(CGSize(width: -140.0, height: -110.0))
+                            .offset(CGSize(width: -130.0, height: 100.0))
                             
                             //Temperature Gauge
+                            let tempGradient = Gradient(colors: [.blue, .red])
+                            
                             VStack {
                                 Gauge(value: engineTemp, in: 0...150) {
                                     Label("Temperature (°F)", systemImage: "thermometer.and.liquid.waves")
-                                    
+                                        
                                 } currentValueLabel: {
                                     Image(systemName: "thermometer.and.liquid.waves")
                                     
                                 } minimumValueLabel: {
-                                    Text("\(Int(0))")
+                                    Text("C")
+                                        .foregroundColor(.blue)
                                     
-                                        .foregroundColor(.green)
                                 } maximumValueLabel: {
-                                    Text("\(Int(150))")
-                                    
+                                    Text("H")
+                                        .foregroundColor(.red)
                                 }
-                                .gaugeStyle(.accessoryCircularCapacity)
-                                
-                                Label("", systemImage: "thermometer.and.liquid.waves")
-                                    .labelStyle(/*@START_MENU_TOKEN@*/DefaultLabelStyle()/*@END_MENU_TOKEN@*/)
+                                .gaugeStyle(.accessoryCircular)
+                                .tint(tempGradient)
+      
                             }
-                            .offset(CGSize(width: 140.0, height: -110.0))
+                            .offset(CGSize(width: 140.0, height: 100.0))
                                
-                            //Gear
+                            //Gear Selector
                             Image(systemName: "1.square.fill")
                                 .resizable()
                                 .scaledToFit()
@@ -179,74 +193,136 @@ struct ContentView: View {
                         accelpedalView()
                     }
                 }
+            
             ZStack (alignment: .topTrailing) {
              
                 // Settings Cog Button with Menu
                 Menu {
-                    Button(action: connectAction) {
-                        Label("Connect", systemImage: "wifi")
-                    }
+                    // Connect Dropdown Menu
+                           Menu {
+                               Button("IP Address") {
+                                   // Action for IP Address
+                                   print("Wi-Fi connect tapped")
+                               }
+                               Button("Port") {
+                                   // Action for Port selection
+                                   print("Port tapped")
+                               }
+                               Button("Protocol") {
+                                   // Action for UDP/TCP switch
+                                   print("Protocol tapped")
+                               }
+                           } label: {
+                               Label("Connect", systemImage: "wifi")
+                           }
+                    
                     Toggle(isOn: $isDarkMode) {
-                        Label("Light/Dark Mode", systemImage: "moon.circle")
+                        Label("Light/Dark Mode", systemImage: isDarkMode ? "moon.fill" : "sun.max.fill")
                     }
                     
                     Toggle(isOn: $isHorizonLockEnabled) {
                         Label("Horizon Lock", systemImage: "lock.rotation")
                     }
+                    
+                    Picker(selection: $selectedUnit, label: Text("Units")) {
+                        Image(systemName: "mph").tag(1)
+                        Image(systemName: "kph").tag(2)
+                    }
+                    .pickerStyle(.automatic) // Optional: Change the picker style
+                    
                 } label: {
                     Image(systemName: "gear")
-                        .font(.system(size: 24))
-                        .padding()
-                        .symbolRenderingMode(.multicolor)
+                        .imageScale(.large)
+                        .symbolRenderingMode(.palette)
                 }
             }
             .offset(CGSize(width: 350.0, height: -80.0))
+            .menuActionDismissBehavior(.disabled)
+            
+            //Info page
+            Button(action: {
+                self.isInfoPopoverPresented = true
+            }, label: {
+                Image(systemName: "info.square")
+                    .imageScale(.large)
+                    .symbolRenderingMode(.hierarchical)
+                
+            })
+            .popover(isPresented: $isInfoPopoverPresented) {
+                      infoView()
+                    .presentationCompactAdaptation(.sheet)
+                    .frame(maxWidth: 400)
+                    .background(.ultraThinMaterial)
+                
+                   }
+            .offset(CGSize(width: -350.0, height: -80.0))
         }
     }
 }
 
 struct SpeedometerGaugeStyle: GaugeStyle {
-    private var heatGradient = LinearGradient(gradient: Gradient(colors: [.blue, .purple, .pink, .red]), startPoint: .trailing, endPoint: .leading)
+    var selectedUnit: Int // Pass as a regular property, not a binding
+    
+    private var heatGradient = LinearGradient(gradient: Gradient(colors: [.teal, .blue, .purple]), startPoint: .trailing, endPoint: .leading)
+    
+    // Public initializer to accept selectedUnit
+        init(selectedUnit: Int) {
+            self.selectedUnit = selectedUnit
+        }
+    
     // Track Orientation
         @State private var isLandscape = true
     func makeBody(configuration: Configuration) -> some View {
         ZStack {
-
             Circle()
-                .foregroundColor(Color(.systemGray6))
+                .foregroundColor(Color(.systemGray4))
                 //.foregroundStyle(.ultraThinMaterial)
-
+            
+            //Stroke colour
             Circle()
                 .trim(from: 0, to: 0.75 * configuration.value)
-                .stroke(heatGradient, style: StrokeStyle(lineWidth: 40.0, lineCap: .round))
+                .stroke(heatGradient, style: StrokeStyle(lineWidth: 15.0, lineCap: .round))
                 .rotationEffect(.degrees(135))
-                .clipShape(Circle())
-            
-
+                .scaleEffect(CGSize(width: 0.85, height: 0.85))
+             
+            //Background stroke colour
             Circle()
                 .trim(from: 0, to: 0.75)
-                .stroke(Color.black, style: StrokeStyle(lineWidth: 10, lineCap: .butt, lineJoin: .round, dash: [1, 34], dashPhase: 0.0))
+                .stroke(heatGradient, style: StrokeStyle(lineWidth: 15.0, lineCap: .round))
                 .rotationEffect(.degrees(135))
+                .scaleEffect(CGSize(width: 0.85, height: 0.85))
+                .opacity(0.2)
+                
+            //Dashes
+         //   Circle()
+        // .trim(from: 0, to: 0.75)
+        // .stroke(Color.black, style: StrokeStyle(lineWidth: 10, lineCap: .butt, lineJoin: .round, dash: [1, 34], dashPhase: 0.0))
+        //   .rotationEffect(.degrees(135))
+        //   .scaleEffect(CGSize(width: 0.95, height: 0.95))
 
+                //Current speed
             VStack {
                 configuration.currentValueLabel
-                    .font(.system(size: 150, weight: .bold, design: .rounded))
-                    .foregroundStyle(.gray)
-                Text("MPH")
-                    .font(.system(.body, design: .rounded))
-                    .bold()
-                    .foregroundStyle(.gray)
-            }
-
+                    .font(.system(size: 80, weight: .bold, design: .rounded))
+                    .foregroundStyle(.primary)
+                
+                // Display selected unit
+                
+                    Image(systemName: "mph")
+                        .font(.system(.body, design: .rounded))
+                        .bold()
+                        .foregroundStyle(selectedUnit == 1 ? Color.gray : Color.black)
+                    
+                    
+                    Image(systemName: "kph")
+                        .font(.system(.body, design: .rounded))
+                        .bold()
+                        .foregroundStyle(selectedUnit == 1 ? Color.black : Color.gray)
+                }
         }
         .frame(width: 280, height: 280)
         .padding(.bottom, -11.0)
     }
-
-}
-
-#Preview {
-    ContentView()
 }
 
 struct brakepedalView: View {
@@ -282,6 +358,7 @@ struct brakepedalView: View {
                         }
                 )
             Text ("Brake")
+                .foregroundStyle(.gray)
         }
     }
 }
@@ -324,6 +401,75 @@ struct accelpedalView: View {
                         }
                 )
             Text ("Accelerator")
+                .foregroundStyle(.gray)
         }
     }
+}
+
+struct infoView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                
+                //App Name
+                Text("About WheelNG")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top)
+                
+                //BeamNG Logo
+                AsyncImage(url: URL(string: "https://wiki.beamng.com/images/thumb/d/d0/BeamNG.drive-logo-2016.svg/744px-BeamNG.drive-logo-2016.svg.png")!, scale: 3) { image in
+                    image.resizable()
+                    image.aspectRatio(contentMode: .fit)
+                    
+                } placeholder: {
+                    Color.orange
+                }
+                .frame(width: 50, height: 100)
+                
+                Text("This is app is a remote control for BeamNG Drive. Allowing users to use their phones as remote controls for BeamNG, using the ")
+                    .padding()
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                //Verison Info
+                Text("Version 1.0.0")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                
+                // Developer Information
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Developed by Stephan Ritchie")
+                        .font(.headline)
+                    
+                    Text("This app is designed to provide a comprehensive and interactive experience for driving simulation enthusiasts. It features customizable speedometers, RPM gauges, and advanced settings to enhance your virtual driving experience.")
+                        .font(.body)
+                        .fixedSize(horizontal: false, vertical: true)
+                }
+                .padding()
+                
+                // Links Section
+                           VStack(alignment: .leading, spacing: 8) {
+                               Text("Follow Us")
+                                   .font(.headline)
+                               
+                               Link("Github", destination: URL(string: "https://github.com/stef1949/WheelNG")!)
+                               
+                               Link("Follow on Twitter", destination: URL(string: "https://x.com/richies3d?s=21")!)
+                               
+                               Link("Like on Facebook", destination: URL(string: "https://www.facebook.com/richies3d")!)
+                           }
+                           .padding(.vertical)
+                           
+                           Spacer()
+                
+            }
+            .padding()
+            .navigationTitle("About")
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
 }
